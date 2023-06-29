f00a8e8f37f964426aa3dccd1d1ad9c26e916dce
Jimmy Rivas <jimminho@iMac-de-Jimmy.local>
2023-06-29 18:23:14 -0500

    iniciojuego

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.juegopyp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.juegopyp to javafx.fxml;
+    exports com.example.juegopyp;
+}
